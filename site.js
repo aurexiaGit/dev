6064bf33d292588207bc5f8835f326f6962e8db2
aurexiaGit <46372862+aurexiaGit@users.noreply.github.com>
2019-01-07 11:53:31 +0100

    Add files via upload

--- site.js
+++ site.js
@@ -1,5 +1,4 @@
 
-
 var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/fb7c0bd217a740f6981ed2274e40d771"))
 
 var TokenABI = web3.eth.contract([
@@ -554,3 +553,5 @@ var Token = TokenABI.at('0x8b0B3674d989980407CD52d2E5F7E3F3F12d372C');
 
 
 var myBalance = Token.balanceOf("0x9a1a785ef4906e1e29e96e3eb5fa4dae8bf4c599").c[0] * 0.0001
+
+document.getElementById("astValue").innerHTML = myBalance.toString() + " AST"
