f8b538d0fbf18e1dadc06eba05fbc4af7e288072
aurexiaGit <46372862+aurexiaGit@users.noreply.github.com>
2019-01-07 10:54:51 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 
-var web3 = new Web3(Web3.currentProvider) 
+var web3 = new Web3(Web3.currentProvider)
+window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/fb7c0bd217a740f6981ed2274e40d771"))
 
 
 // Import the contract artifact and turn it into usable an abstraction
@@ -11,3 +12,8 @@ function sayHello() {
   }).then(function(result) { 
     watchingForEvents = true;
 })}
+
+web3.eth.getBalance("0x9a1a785ef4906e1e29e96e3eb5fa4dae8bf4c599", (err, balance) => {
+    const balanceInEth = web3.fromWei(balance, "ether");
+    console.log(`balance ${balanceInEth} ether`);
+  });
