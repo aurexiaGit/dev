0498361e76b70f52204b5526ca9a49bfd4a53139
acavelius <cavelius.antoine@gmail.com>
2019-01-10 15:36:55 +0100

    ethereumConnexionProblem2

--- scripts/site.js
+++ scripts/site.js
@@ -688,7 +688,9 @@ function getBalance(Account) {
 function createPage() {
 	myBalance()
 	makeGraph()
-	document.getElementById("astValue").innerHTML = Balance.toString() + " AST"
+	if (document.getElementById("astValue") !== undefined) {
+		document.getElementById("astValue").innerHTML = Balance.toString() + " AST"	
+	}
 	var curAccount = web3.eth.accounts[0]
 	for (var key in users){
 		if (users.hasOwnProperty(key) && users[key].adress.toLowerCase()===curAccount.toLowerCase()) {
