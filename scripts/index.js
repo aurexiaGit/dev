d4b900e07e28b919e97aa0255ec214cd0f961cef
acavelius <cavelius.antoine@gmail.com>
2019-01-10 15:51:04 +0100

    seperationIntoTwoJSFiles

---
+++ scripts/index.js
@@ -0,0 +1,711 @@
+var users = {
+			"admin":{
+			"password": "admin",
+			"adress": "0x9a1a785eF4906e1E29E96E3eb5Fa4daE8bf4c599",
+			"name": "Antoine CAVELIUS",
+			"pic":"images/acavelius.png"
+			},
+			"dominique":{
+			"password": "user2",
+			"adress": "",
+			"name": "Dominique HERROU",
+			"pic":"images/dherrou.jpg"
+			},
+			"eric":{
+			"password": "user2",
+			"adress": "",
+			"name": "Eric VERNHES",
+			"pic":"images/evernhes.png"
+			},
+			"david":{
+			"password": "user2",
+			"adress": "",
+			"name": "David VILLARD",
+			"pic":"images/dvillard.jpg"
+			},
+			"charles":{
+			"password": "user2",
+			"adress": "",
+			"name": "Charles BAIN DE LA COCQUERIE",
+			"pic":"images/cbain.jpg"
+			}
+		}
+
+
+var logged;
+var user;
+var pass;
+
+function Login(){
+	if (username.value in users && users[username.value].password === password.value)  {
+		user = 'user1'
+		pass = 'user1'
+		logged = true;
+		window.location="homePage.html";
+	}
+	return false
+}
+
+if (logged) {
+	document.getElementById("nom_utilisateur").value = user
+}
+
+function logOut() {
+	window.location="login.html"
+}
+
+function homePage() {
+	window.location="index.html"
+}
+
+function adminPage() {
+	window.location="admin.html"
+}
+
+function eventsPage() {
+	window.location="events.html"
+}
+
+
+window.onload=makeGraph;
+
+document.getElementById("adminPage").style.display = "none"
+
+if (window.ethereum===undefined) {
+	if(window.confirm('You need to use google Chrome and have Metamask installed ! Click "ok" to start dowloading.')) 
+		{
+		window.location.href='https://metamask.io';
+	}
+}
+else {
+		window.ethereum
+		ethereum.enable()
+}
+
+
+
+var web3 = new Web3(web3.currentProvider)
+
+var TokenABI = web3.eth.contract([
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newSellPrice",
+				"type": "uint256"
+			},
+			{
+				"name": "newBuyPrice",
+				"type": "uint256"
+			}
+		],
+		"name": "setPrices",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "initialSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "burn",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "sellPrice",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"name": "mintedAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "mintToken",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "burnFrom",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "buyPrice",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "buy",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"name": "member",
+				"type": "bool"
+			}
+		],
+		"name": "assignMember",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "frozenAccount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			},
+			{
+				"name": "_extraData",
+				"type": "bytes"
+			}
+		],
+		"name": "approveAndCall",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "sell",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"name": "freeze",
+				"type": "bool"
+			}
+		],
+		"name": "freezeAccount",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "aurexiaMember",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "frozen",
+				"type": "bool"
+			}
+		],
+		"name": "FrozenFunds",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "member",
+				"type": "bool"
+			}
+		],
+		"name": "IsMember",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Burn",
+		"type": "event"
+	}
+]);
+
+var Token = TokenABI.at('0x8b0B3674d989980407CD52d2E5F7E3F3F12d372C');
+
+var curAccount = web3.eth.accounts[0]
+
+var select = document.getElementById("dest-select")
+
+for (var key in users){
+	if (users.hasOwnProperty(key) && key !== "admin") {
+		var opt = document.createElement('option');
+	    opt.value = users[key].adress;
+	    opt.innerHTML = users[key].name;
+	    select.appendChild(opt);
+	}
+}
+
+var Balance
+
+function myBalance() {
+	var curAccount = web3.eth.accounts[0]
+	Token.balanceOf(curAccount, function(err,result) {
+		if (!err) {Balance = result.c[0]*0.0001 ; console.log("")}
+	})
+}
+
+function getBalance(Account) {
+	var Account = Account
+	Token.balanceOf(this.Account, function(err,result) {
+		if (!err) {Balance = result.c[0]*0.0001 ; console.log("")}
+	})
+	return Balance
+}
+
+function createPage() {
+	myBalance()
+	if (document.getElementById("astValue") !== undefined) {
+		document.getElementById("astValue").innerHTML = Balance.toString() + " AST"	
+	}
+	var curAccount = web3.eth.accounts[0]
+	for (var key in users){
+		if (users.hasOwnProperty(key) && users[key].adress.toLowerCase()===curAccount.toLowerCase()) {
+			var identity = document.getElementById("identity");
+			identity.innerHTML="Welcome to your Wallet <br>" + users[key].name + "! <br> <img class = 'pic' src='" + users[key].pic + "' alt='profile pic'> "
+		}
+		if (users.hasOwnProperty(key) && key==="admin") {
+			document.getElementById("adminPage").style.display = "inline-block"
+		}
+	}
+}
+
+
+window.setInterval(function() {
+	createPage()
+}, 1000);
+
+
+function sendToken(adress,amount) {
+	Token.transfer(adress,parseInt(web3.toWei(amount.toString(),"ether")),function(err,result) {console.log("")})
+}
+
+function Transfer() {
+	sendToken(document.getElementById("dest-select").value,document.getElementById("amount").value)
+	var frm = document.getElementById("send");
+	frm.reset();
+	return false
+}
+
+function newMember(adress) {
+	Token.assignMember(adress,true,function(err,result) {console.log("")})
+}
+
+function Member() {
+	newMember(document.getElementById("adress").value)
+	var frm = document.getElementById("member");
+	frm.reset();
+	return false
+}
