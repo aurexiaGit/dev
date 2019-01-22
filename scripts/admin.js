cd893750541fb6446c54b3035c934e7fb14f6034
Antoine CAVELIUS <acavelius@aurexia.local>
2019-01-22 17:52:52 +0100

    donation not to admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -679,7 +679,9 @@ function mintTokens(adress,amount) {
 function initialDonations() {
 	if (window.confirm("Do you really want to send initial donations to all Blockchain members?")) {
 		for (key in users) {
-			mintTokens(users[key].adress,40)
+			if (key !== "admin") {
+				mintTokens(users[key].adress,40)
+			}
 		}
 	}
 }
