3496797a16566f73511539fe6a7c4dcfeac1a556
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-03 16:26:42 +0200

    test account main

--- AST_site_testSP/scripts/main.js
+++ AST_site_testSP/scripts/main.js
@@ -769,6 +769,8 @@ var Token = TokenABI.at('0x9D370c0bEfd7Dab940EEF7783D942cff020B15B9');
 // get current account on metamask
 
 var curAccount = web3.eth.accounts[0];
+console.log (web3.eth.accounts[0])
+console.log (web3.eth.accounts[1])
 
 var addressOwner = Token.owner(async function (err, res){addressOwner = await res});
 
