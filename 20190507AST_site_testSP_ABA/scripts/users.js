5e5a04e7907ef1f6a31ae8933e4642fcca30547e
Amine Badry <amine.badry@gmail.com>
2019-05-07 14:16:55 +0800

    Creation AST_site_testSPAABAV2

--- 20190507AST_site_testSP_ABA/scripts/users.js
+++ 20190507AST_site_testSP_ABA/scripts/users.js
@@ -17,37 +17,59 @@ var charity = {
 			}
 }
 
-const getUsersInfo = async () =>{
+const getUsers = async () =>{
 
 	let listAddress;
 	let name;
+	var i = 0;
   
 	const getMembers = () =>{                        
-	  return new Promise(function(resolve, reject){
-		Token.getMembers((err, members) => {
-		  if (err) return reject(err);
-		  resolve(members);
-	  })
+		return new Promise(function(resolve, reject){
+			Token.getMembers((err, members) => {
+				if (err) return reject(err);
+				resolve(members);
+	  	})
 	})}
-
+	/*
 	const getName = () =>{                        
 		return new Promise(function(resolve, reject){
-		  Token.getName((err, name) => {
-			if (err) return reject(err);
-			resolve(name);
+			Token.getName((err, name) => {
+				if (err) return reject(err);
+				resolve(name);
 		})
 	})}	
-  
+	*/
+
+	const getName = (address) =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getName(address, (err, name) => {
+				if (err) return reject(err);
+				resolve(name);
+		})
+	})}	
+
 	listAddress = await getMembers();
 	console.log("get list of addresses")
 	console.log(listAddress);
-	name = await getName();
-	console.log("get names")
-	console.log(name);
+	while (i < listAddress.length) {
+		var address = listAddress[i];
+		console.log(address)
+		name = await getName(address);
+		users[name]={}
+		users[name].address=address
+		users[name].name=name
+		i++
+		console.log(users[name].address)
+		console.log(users[name].name)
+	}
+	//name = await getName();
+	//console.log("get names")
+	//console.log(name);
 
-	return getUsers(listAddress, name);
+	//return getUsers(listAddress, name);
 };
 
+/*
 const getUsers = (_listAddress, _name) => {
 	var i = 0
 	while (i < listAddress.length) {
@@ -60,5 +82,28 @@ const getUsers = (_listAddress, _name) => {
 		console.log(users[name].name)
 	}
 };
+*/
+getUsers();
 
-getUsersInfo();
+/*
+function getUsers() {
+	// The function gets the array of addresses from the Blockchain and then fills the dictionnary users
+	var i = 0
+	var name
+	Token.getMembers(function(err,result) {listAddress = result})
+	// To get the information the js file must comunicate with the Blockchain. This takes time.
+	// Thus, we need to create this embedded function to wait between each calls.
+	function getUser() {
+		var address = listAddress[i]
+		Token.getName(address,function(err,result) {name = result})
+		window.setTimeout(function () {
+			users[name]={}
+			users[name].address=address
+			users[name].name=name
+			i++
+			if (i < listAddress.length) {getUser()}
+		},1500)
+	}
+	window.setTimeout(function() {getUser()},1500)
+}
+*/
