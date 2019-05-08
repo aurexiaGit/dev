94c33187d727af7016e0acd134b446aa97eae86e
Amine Badry <amine.badry@gmail.com>
2019-05-08 17:15:04 +0800

    Creation AST_site_testSPAABAV2

--- 20190507_SPI_ABA/scripts/history.js
+++ 20190507_SPI_ABA/scripts/history.js
@@ -54,58 +54,61 @@ const getHistory = async () =>{
 		console.log(users)
 		var resultArray = data.result
 
-		var table = document.getElementById("content-history")
-		var i = 1
-
-		for (var key in resultArray){
-
-			var row = document.createElement('tr')
-			row.class = "row" + i.toString() + " body"
-			table.appendChild(row)
-
-			var column1 = document.createElement('td')
-			column1.className = "column1"
-			column1.innerHTML = resultArray[key].timeStamp
-			row.appendChild(column1)
-			console.log(resultArray[key].timeStamp)
-
-			var column2 = document.createElement('td')
-			column2.className = "column2"
-			if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
-				column2.innerHTML = "Reception"
-			}
-			else {
-				column2.innerHTML = "Transfer"
-			}
-			row.appendChild(column2)
-			console.log(column2.innerHTML)
-
-			var column3 = document.createElement('td')
-			column3.className = "column3"
-			column3.innerHTML = await getName(resultArray[key].from)
-			console.log(column3.innerHTML)
-			row.appendChild(column3)
-
-			var column4 = document.createElement('td')
-			column4.className = "column4"
-			column4.innerHTML = await getName(resultArray[key].to)
-			row.appendChild(column4)
-			console.log(column4.innerHTML)
+		const fillHistory = async (resultArray) =>{
 			
-			var column5 = document.createElement('td')
-			column5.className = "column5"
-			if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
-				column5.innerHTML = "+" + resultArray[key].amount
-			}
-			else {
-				column5.innerHTML = "+" + resultArray[key].amount
-			}
-			row.appendChild(column5)
-			console.log(column5.innerHTML)
+			var table = document.getElementById("content-history")
+			var i = 1
+			for (var key in resultArray){
 
-			i++
+				var row = document.createElement('tr')
+				row.class = "row" + i.toString() + " body"
+				table.appendChild(row)
 
+				var column1 = document.createElement('td')
+				column1.className = "column1"
+				column1.innerHTML = resultArray[key].timeStamp
+				row.appendChild(column1)
+				console.log(resultArray[key].timeStamp)
+
+				var column2 = document.createElement('td')
+				column2.className = "column2"
+				if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
+					column2.innerHTML = "Reception"
+				}
+				else {
+					column2.innerHTML = "Transfer"
+				}
+				row.appendChild(column2)
+				console.log(column2.innerHTML)
+
+				var column3 = document.createElement('td')
+				column3.className = "column3"
+				column3.innerHTML = await getName(resultArray[key].from)
+				console.log(column3.innerHTML)
+				row.appendChild(column3)
+
+				var column4 = document.createElement('td')
+				column4.className = "column4"
+				column4.innerHTML = await getName(resultArray[key].to)
+				row.appendChild(column4)
+				console.log(column4.innerHTML)
+				
+				var column5 = document.createElement('td')
+				column5.className = "column5"
+				if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
+					column5.innerHTML = "+" + resultArray[key].amount
+				}
+				else {
+					column5.innerHTML = "+" + resultArray[key].amount
+				}
+				row.appendChild(column5)
+				console.log(column5.innerHTML)
+
+				i++
+
+			}
 		}
+		fillHistory(resultArray);
 	});
 
 };
