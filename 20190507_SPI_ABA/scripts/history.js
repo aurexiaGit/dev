919b10e66caef59cb4058fc7fe58e3c9a6aa21b1
Amine Badry <amine.badry@gmail.com>
2019-05-08 16:49:53 +0800

    Creation AST_site_testSPAABAV2

--- 20190507_SPI_ABA/scripts/history.js
+++ 20190507_SPI_ABA/scripts/history.js
@@ -1,3 +1,61 @@
+const getLogHistory = async () =>{
+
+	let curAddress;
+	let ownerAddress;
+  
+	const getCurAddress = () =>{                         
+	  return new Promise(function(resolve, reject){
+		web3.eth.getAccounts((err, accounts) => {
+		  if (err) return reject(err);
+		  resolve(accounts[0]);
+	  })
+	})}
+  
+	curAddress = await getCurAddress();
+	return curAddress;
+  };
+
+const getUsersHistory = async () =>{
+
+	let users = {};
+	let listAddress;
+	let name;
+	var i = 0;
+
+	const getMembers = () =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getMembers((err, members) => {
+				if (err) return reject(err);
+				resolve(members);
+		})
+	})}
+
+	const getName = (address) =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getName(address, (err, name) => {
+				if (err) return reject(err);
+				resolve(name);
+		})
+	})}	
+
+	listAddress = await getMembers();
+	console.log("get list of addresses")
+	console.log(listAddress);
+	while (i < listAddress.length) {
+		var address = listAddress[i];
+		console.log(address)
+		name = await getName(address);
+		users[name]={}
+		users[name].address=address
+		users[name].name=name
+		i++
+		console.log(users[name].address)
+		console.log(users[name].name)
+	}
+
+	return users;
+};
+
 address = getLogHistory();
 
 $.getJSON('https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + address + '&startblock=0&endblock=999999999&sort=asc&apikey=NSAMUW521D6CQ63KHUPRQEERSW8FVRAF9B', function(data) {
@@ -59,60 +117,3 @@ $.getJSON('https://api-ropsten.etherscan.io/api?module=account&action=tokentx&ad
 	}
 });
 
-const getLogHistory = async () =>{
-
-	let curAddress;
-	let ownerAddress;
-  
-	const getCurAddress = () =>{                         
-	  return new Promise(function(resolve, reject){
-		web3.eth.getAccounts((err, accounts) => {
-		  if (err) return reject(err);
-		  resolve(accounts[0]);
-	  })
-	})}
-  
-	curAddress = await getCurAddress();
-	return curAddress;
-  };
-
-const getUsersHistory = async () =>{
-
-	let users = {};
-	let listAddress;
-	let name;
-	var i = 0;
-
-	const getMembers = () =>{                        
-		return new Promise(function(resolve, reject){
-			Token.getMembers((err, members) => {
-				if (err) return reject(err);
-				resolve(members);
-		})
-	})}
-
-	const getName = (address) =>{                        
-		return new Promise(function(resolve, reject){
-			Token.getName(address, (err, name) => {
-				if (err) return reject(err);
-				resolve(name);
-		})
-	})}	
-
-	listAddress = await getMembers();
-	console.log("get list of addresses")
-	console.log(listAddress);
-	while (i < listAddress.length) {
-		var address = listAddress[i];
-		console.log(address)
-		name = await getName(address);
-		users[name]={}
-		users[name].address=address
-		users[name].name=name
-		i++
-		console.log(users[name].address)
-		console.log(users[name].name)
-	}
-
-	return users;
-};
