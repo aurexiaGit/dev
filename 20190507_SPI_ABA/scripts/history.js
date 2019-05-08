626128d0184f6c80ed949e008339a3845253eda0
Amine Badry <amine.badry@gmail.com>
2019-05-08 16:56:58 +0800

    Creation AST_site_testSPAABAV2

--- 20190507_SPI_ABA/scripts/history.js
+++ 20190507_SPI_ABA/scripts/history.js
@@ -1,4 +1,4 @@
-const getLogHistory = async () =>{
+const getHistory = async () =>{
 
 	let curAddress;
 	let ownerAddress;
@@ -12,11 +12,7 @@ const getLogHistory = async () =>{
 	})}
   
 	curAddress = await getCurAddress();
-	return curAddress;
-  };
-
-const getUsersHistory = async () =>{
-
+	
 	let users = {};
 	let listAddress;
 	let name;
@@ -53,67 +49,64 @@ const getUsersHistory = async () =>{
 		console.log(users[name].name)
 	}
 
-	return users;
+	$.getJSON('https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + curAddress + '&startblock=0&endblock=999999999&sort=asc&apikey=NSAMUW521D6CQ63KHUPRQEERSW8FVRAF9B', function(data) {
+		console.log(data)
+		console.log(users)
+		var resultArray = data.result
+
+		var table = document.getElementById("content-history")
+		var i = 1
+
+		for (var key in resultArray){
+
+			var row = document.createElement('tr')
+			row.class = "row" + i.toString() + " body"
+			table.appendChild(row)
+
+			var column1 = document.createElement('td')
+			column1.className = "column1"
+			column1.innerHTML = resultArray[key].timeStamp
+			row.appendChild(column1)
+			console.log(resultArray[key].timeStamp)
+
+			var column2 = document.createElement('td')
+			column2.className = "column2"
+			if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
+				column2.innerHTML = "Reception"
+			}
+			else {
+				column2.innerHTML = "Transfer"
+			}
+			row.appendChild(column2)
+			console.log(column2.innerHTML)
+
+			var column3 = document.createElement('td')
+			column3.className = "column3"
+			column3.innerHTML = users[resultArray[key].from].name
+			row.appendChild(column3)
+			console.log(users[resultArray[key].from].name)
+
+			var column4 = document.createElement('td')
+			column4.className = "column4"
+			column4.innerHTML = users[resultArray[key].to].name
+			row.appendChild(column4)
+			console.log(users[resultArray[key].to].name)
+			
+			var column5 = document.createElement('td')
+			column5.className = "column5"
+			if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
+				column5.innerHTML = "+" + resultArray[key].amount
+			}
+			else {
+				column5.innerHTML = "+" + resultArray[key].amount
+			}
+			row.appendChild(column5)
+			console.log(column5.innerHTML)
+
+			i++
+
+		}
+	});
+
 };
-
-address = getLogHistory();
-
-$.getJSON('https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + address + '&startblock=0&endblock=999999999&sort=asc&apikey=NSAMUW521D6CQ63KHUPRQEERSW8FVRAF9B', function(data) {
-	users = getUsersHistory();
-	console.log(users)
-	var resultArray = data.result
-
-	var table = document.getElementById("content-history")
-	var i = 1
-
-	for (var key in resultArray){
-
-		var row = document.createElement('tr')
-		row.class = "row" + i.toString() + " body"
-		table.appendChild(row)
-
-		var column1 = document.createElement('td')
-		column1.className = "column1"
-		column1.innerHTML = resultArray[key].timeStamp
-		row.appendChild(column1)
-		console.log(resultArray[key].timeStamp)
-
-		var column2 = document.createElement('td')
-		column2.className = "column2"
-		if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
-			column2.innerHTML = "Reception"
-		}
-		else {
-			column2.innerHTML = "Transfer"
-		}
-		row.appendChild(column2)
-		console.log(column2.innerHTML)
-
-		var column3 = document.createElement('td')
-		column3.className = "column3"
-		column3.innerHTML = users[resultArray[key].from].name
-		row.appendChild(column3)
-		console.log(users[resultArray[key].from].name)
-
-		var column4 = document.createElement('td')
-		column4.className = "column4"
-		column4.innerHTML = users[resultArray[key].to].name
-		row.appendChild(column4)
-		console.log(users[resultArray[key].to].name)
-		
-		var column5 = document.createElement('td')
-		column5.className = "column5"
-		if (resultArray[key].from == "0xc4d446c6B924c431f89214319D5A3e6bb67e7627") {
-			column5.innerHTML = "+" + resultArray[key].amount
-		}
-		else {
-			column5.innerHTML = "+" + resultArray[key].amount
-		}
-		row.appendChild(column5)
-		console.log(column5.innerHTML)
-
-		i++
-
-	}
-});
 
