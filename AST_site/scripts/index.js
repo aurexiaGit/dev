8e9cc5a3ef6de1d936baa538f255c796123229a8
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-09 14:05:15 +0200

    new index

--- AST_site/scripts/index.js
+++ AST_site/scripts/index.js
@@ -1,6 +1,7 @@
 // ******************************************* 
 //Test update amount value after sending tokens
-const getAccountAddress= async () => {
+
+const refreshBalance= async () => {
 
 	const getCurAddress = () =>{                         
 		return new Promise(function(resolve, reject){
@@ -10,7 +11,6 @@ const getAccountAddress= async () => {
 		})
   	})};
 	
-	/*
   	const getBalance = (_curAddress) =>{
 		return new Promise(function(resolve, reject){
 			Token.balanceOf(_curAddress, (err, result) => {
@@ -19,44 +19,98 @@ const getAccountAddress= async () => {
 			})
 		})
 	};
-	*/
+	
 	let curAddress = await getCurAddress();
-	//let balance = await getBalance(curAddress);
-	//let accountInfo = [curAddress, balance];
+	let balance = await getBalance(curAddress);
+
+	//var today to delete
+	var today = new Date();
+	var time;
+
+	const filter = web3.eth.filter('latest');
+	filter.watch((err, res) => {
+	if (err) {
+		console.log(`Watch error: ${err}`);
+	} else {
+	// Update balance
+		const refresh= async (curAddress,balance) => {
+			let bal;
+			bal = await getBalance(curAddress);
+			time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			console.log("Real account balance at " + time + ": " + bal)
+			if (balance > bal) {
+				alert("Your transaction has been executed!")
+				balance = bal;
+				console.log("Transaction has been executed, balance is " + balance);
+				document.getElementById("astValue").innerHTML = balance.toString() + " AST"
+				balance = await getBalance(curAddress)
+				bal = await getBalance(curAddress)
+			} else if (balance < bal) {
+				alert("You have received " + (balance - bal).toString() + " AST!")
+				balance = bal;
+				console.log("Reception of tokens, balance is " + balance);
+				document.getElementById("astValue").innerHTML = balance.toString() + " AST"
+				balance = await getBalance(curAddress)
+				bal = await getBalance(curAddress)
+			}
+			//return balance;
+		}
+		refresh(curAddress,balance);
+		//balance = refresh(curAddress, balance);
+	}
+	});
+}
+
+refreshBalance();
+/*
+const getAccountAddress= async () => {
+
+	let curAddress;
+
+	const getCurAddress = () =>{                         
+		return new Promise(function(resolve, reject){
+		web3.eth.getAccounts((err, accounts) => {
+			if (err) return reject(err);
+			console.log(accounts[0])
+			resolve(accounts[0]);
+		})
+  	})};
+	curAddress = await getCurAddress();
+	console.log(curAddress)
 	return curAddress;
-	//return accountInfo;
-	//return balance;
 }
 
 const getAccountBalance= async (_curAddress) => {
-	
+
+	let balance;
+
   	const getBalance = (_curAddress) =>{
 		return new Promise(function(resolve, reject){
 			Token.balanceOf(_curAddress, (err, result) => {
 				if (err) return reject (err);
+				console.log(result*Math.pow(10,-18))
 				resolve(result*Math.pow(10,-18));
 			})
 		})
 	};
-	let balance = await getBalance(_curAddress);
+	balance = await getBalance(_curAddress);
+	console.log(balance)
 	return balance;
 }
 
-//var accountInfo = getAccountInfo();
 var curAddress = getAccountAddress();
-//var curAddress = accountInfo[0]
-var balance = getAccountBalance(curAddress);
-//var balance = accountInfo[1]
 console.log(curAddress)
+//var balance = getAccountBalance(curAddress);
+balance = 17449999999999996995997
 console.log(balance)
 
 const filter = web3.eth.filter('latest');
 filter.watch((err, res) => {
-  if (err) {
-    console.log(`Watch error: ${err}`);
-  } else {
-    // Update balance
-    Token.balanceOf(curAddress, (err, bal) => {
+	if (err) {
+		console.log(`Watch error: ${err}`);
+	} else {
+	// Update balance
+    Token.balanceOf("0xc4d446c6B924c431f89214319D5A3e6bb67e7627", (err, bal) => {
       if (err) {
         console.log(`getBalance error: ${err}`);
       } else {
@@ -74,10 +128,10 @@ filter.watch((err, res) => {
 			document.getElementById("astValue").innerHTML = balance.toString() + " AST"
 		}
       }
-    });
-  }
+	});
+}
 });
-
+*/
 // *******************************************
 
 function createPage(Balance) {	
