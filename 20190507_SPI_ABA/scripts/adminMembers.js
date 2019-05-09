683b2f9652981c903aad15a7c530f1ff1a97eedd
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-09 17:55:06 +0200

    add people

--- 20190507_SPI_ABA/scripts/adminMembers.js
+++ 20190507_SPI_ABA/scripts/adminMembers.js
@@ -3,24 +3,16 @@ const resetAdd = () => {
 	frm.reset();
 }
 
-
 const addMember = async () => {
 	// Called when clicking on Add button
 	var _address = document.getElementById("adress1").value
 	var _name = document.getElementById("name1").value
 	var _grade = document.getElementById ("grade1").value
 
-	const addM = (address,name,grade) =>{                         
-		return new Promise(function(resolve, reject){
-			Token.addToAurexiaMembers(address,name,grade,(err,result) => {
-				if (err) return reject(err);
-				resolve(result);
-			})
-	  	})
-	};
-	let assigment = await addM(_address,_name,_grade)
-	console.log(assigment)
-	return resetAdd()
+	Token.addToAurexiaMembers(_address,_name,_grade,(err,result) => {console.log(result)})
+	var frm = document.getElementById("remMember");
+	frm.reset();
+	return true;
 }
 
 function remMember() {
