65c44ccfa34289dceba05c8be5fb3e94f04e8d8d
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-09 16:42:36 +0200

    minor changes

--- AST_site/scripts/index.js
+++ AST_site/scripts/index.js
@@ -200,9 +200,9 @@ const Transfer = async() => {
 		})
 	};
 
+	transferTransaction = await transferEvent(address,amount);
 	var frm = document.getElementById("send");
 	frm.reset();
-	transferTransaction = await transferEvent(address,amount);
 	return transferTransaction
 }
 	
