9fddef6b4622ae7602076d7dd30a1b9d6f7432d3
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-10 10:48:49 +0200

    ajout sc

---
+++ AST_site/TruffleSmartContract/truffle-config.js
@@ -0,0 +1,17 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const memonic = "enhance prefer gospel staff reward youth forward curve before predict crouch bottom"
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(memonic, "https://rinkeby.infura.io/v3/cc89e42528e441afb25d84e1499632ba")
+      },
+      network_id: 4
+    }
+  }
+};
