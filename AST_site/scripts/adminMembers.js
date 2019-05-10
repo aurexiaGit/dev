ff3eff9cc2904f7b6cdaaaade99322da412a4243
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-10 10:30:53 +0200

    ajout.rem people

--- AST_site/scripts/adminMembers.js
+++ AST_site/scripts/adminMembers.js
@@ -1,24 +1,58 @@
-function addMember() {
+const addMember = async () => {
 	// Called when clicking on Add button
 	var _address = document.getElementById("adress1").value
 	var _name = document.getElementById("name1").value
 	var _grade = document.getElementById ("grade1").value
-	newMembre = {};
-	newMembre.name = _name;
-	newMembre.address = _address;
-	newMembre.grade = _grade;
-	Token.addToAurexiaMembers(_address,_name,_grade,function(err,result) {console.log("")})
-	users[_name] = newMembre;
+
+	const getCurAddress = () =>{                         
+		return new Promise(function(resolve, reject){
+		web3.eth.getAccounts((err, accounts) => {
+			if (err) return reject(err);
+			let result = accounts[0].toLowerCase();
+			resolve(result);
+		})
+  	})};
+
+	const addM = (address, name, grade, curAddress) =>{                         
+		return new Promise(function(resolve, reject){
+			Token.addToAurexiaMembers.sendTransaction(address,name,grade,{from:curAddress},(err,result) => {
+				if (err) return reject(err);
+				resolve(result);
+			})
+	  	})
+	};
+	let _curAddress = await getCurAddress();
 	var frm = document.getElementById("addMember");
 	frm.reset();
-	return false
+	let assigment = await addM(_address,_name,_grade, _curAddress)
+	console.log(assigment)
+	return false;
 }
 
 function remMember() {
 	// Called when clicking on remove button
-	var address = document.getElementById("adress2").value
-	Token.remAurexiaMembers(address,function(err,result) {console.log("")})
+	var _address = document.getElementById("adress2").value
+	const getCurAddress = () =>{                         
+		return new Promise(function(resolve, reject){
+		web3.eth.getAccounts((err, accounts) => {
+			if (err) return reject(err);
+			let result = accounts[0].toLowerCase();
+			resolve(result);
+		})
+  	})};
+
+	const remM = (address, curAddress) =>{                         
+		return new Promise(function(resolve, reject){
+			Token.remAurexiaMembers.sendTransaction(address,{from:curAddress},(err,result) => {
+				if (err) return reject(err);
+				resolve(result);
+			})
+	  	})
+	};
+	let _curAddress = await getCurAddress();
 	var frm = document.getElementById("remMember");
 	frm.reset();
-	return false
+	let assigment = await remM(_address, _curAddress)
+	console.log(assigment)
+	return false;
 }
