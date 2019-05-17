60198bad1c68c319a77c5b20f597862283c6ebe0
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-17 15:26:21 +0200

    ajout blockchain test

---
+++ Blockchain_vTest/scripts/adminList.js
@@ -0,0 +1,112 @@
+
+////////////////////////////////////////////////////////
+//     Creation de la table listant les users         //
+////////////////////////////////////////////////////////
+
+const getUserTable = (_users, _taille) => {
+	var table = document.getElementById("content");
+	var i = 1;
+
+	for (var k=0; k<_taille; k++){
+
+		var row = table.insertRow(-1);
+		row.className = "row" + i.toString() + " body";
+
+		var column1 = document.createElement('td');
+		column1.className = "column1";
+		column1.innerHTML = i;
+		row.appendChild(column1);
+
+		var column2 = document.createElement('td');
+		column2.className = "column2";
+		column2.innerHTML = _users[k].name;
+		row.appendChild(column2);
+
+		var column3 = document.createElement('td');
+		column3.className = "column3";
+		column3.innerHTML = _users[k].address;
+		row.appendChild(column3);
+
+		var column4 = document.createElement('td');
+		column4.className = "column4";
+		column4.innerHTML = Math.round(_users[k].balance);
+		row.appendChild(column4);
+
+		i++;
+	}
+}
+
+const getUsersList = async () =>{
+
+	let users = {};
+	let listAddress;
+	let name;
+	let i = 0;
+  
+	const getMembers = async () =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getMembers((err, members) => {
+				if (err) return reject(err);
+				resolve(members);
+	  		})
+		})
+	};
+
+	const getName = async (address) =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getName(address, (err, name) => {
+				if (err) return reject(err);
+				resolve(name);
+			})
+		})
+	};
+	
+	const getBalance = async (_curAddress) =>{
+		return new Promise(function(resolve, reject){
+			Token.balanceOf(_curAddress, (err, result) => {
+				if (err) return reject (err);
+				resolve(result*Math.pow(10,-18));
+			})
+		})
+	};
+
+	const getTaille = async () =>{
+		return new Promise(function(resolve, reject){
+		  Token.sizeListAccount((err, result) => {
+			if (err) return reject(err);
+			resolve(result);
+		})
+	  })}
+
+	listAddress = await getMembers();
+	let taille = await getTaille();
+	console.log("get list of addresses")
+	console.log(listAddress);
+	while (i < taille) {
+		var address = listAddress[i];
+		name = await getName(address);
+		balance = await getBalance(address);
+		users[i]={};
+		users[i].address=address;
+		users[i].name=name;
+		users[i].balance=balance;
+		i++;
+	}
+
+	console.log(users);
+
+	for (var k = taille-1; k > 0 ; k--){
+		for (var j = 0; j < k; j++){
+			if (users[j].balance < users[j+1].balance){
+				users["tempo"] = users[j];
+				users[j] = users[j+1];
+				users[j+1] = users["tempo"];
+			}
+		}
+	}
+	console.log(users);
+
+	return getUserTable(users, taille)
+};
+
+getUsersList();
---
+++ Blockchain_vTest/scripts/main.js
@@ -0,0 +1,945 @@
+
+// for the "sending tokens" loading sentence
+
+var sending = false; 
+
+// hide admin logo 
+
+document.getElementById("adminPage").style.display = "none";
+
+// hide notif banner
+
+var show = false;
+var elmt = document.getElementById("notifBanner");
+elmt.style.display = "none";
+
+// connect to ethereum API web3
+
+var web3 = new Web3(web3.currentProvider);
+
+// get token as a variable
+
+var TokenABI = web3.eth.contract([
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "sizeListCharity",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "openDonation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "initialSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "_totalSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "acceptOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "newOwner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "sizeListAccount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "tokens",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Burn",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "_to",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "sender",
+        "type": "address"
+      },
+      {
+        "name": "spender",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_receiver",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "mint",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "burn",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferAll",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "name": "_grade",
+        "type": "uint8"
+      }
+    ],
+    "name": "addToAurexiaMembers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "remAurexiaMembers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getMembers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "getAddress",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "isInAurexiaMembers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "getName",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "getGrade",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "name": "newGrade",
+        "type": "uint8"
+      }
+    ],
+    "name": "modifyGrade",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "name": "newName",
+        "type": "string"
+      }
+    ],
+    "name": "modifyName",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      },
+      {
+        "name": "_name",
+        "type": "string"
+      }
+    ],
+    "name": "addAssociation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "remAssociation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "transferToAssociation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getCharityAddress",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getCharitySize",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "launchDonation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "closeDonation",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "isAssoPartner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "getAssoName",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]);
+
+var Token = TokenABI.at('0x98648B49dC72Cf832DA9A771Db38e55b0d8bF56A');
+
+// check that user has Metamask installed 
+
+if (window.ethereum===undefined) {
+	window.confirm('Pas installe'); 
+}
+else {
+  window.ethereum
+  ethereum.enable()
+}
+
+
+
+const getLog = async () =>{
+
+  let curAddress;
+  let ownerAddress;
+
+  const getCurAddress = async () =>{                         // fonctionne mais on a besoin de reloader la page pour que ca s'initialise (le await ne marche pas pour la fonction getAccounts de web3)
+    return new Promise(function(resolve, reject){
+      web3.eth.getAccounts((err, accounts) => {
+        if (err) return reject(err);
+        resolve(accounts[0]);
+    })
+  })}
+
+  const getOwner = async () =>{
+    return new Promise(function(resolve, reject){
+      Token.owner((err, accounts) => {
+        if (err) return reject(err);
+        resolve(accounts);
+    })
+  })}
+
+  const getName = async (address) =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getName(address, (err, name) => {
+				if (err) return reject(err);
+				resolve(name);
+			})
+		})
+	}
+
+  curAddress = await getCurAddress();
+  ownerAddress = await getOwner();
+  curName = await getName(curAddress);
+
+  return getBanner(curAddress, ownerAddress, curName);
+};
+
+const getBanner = (_curAddress, _ownerAddress, _name) => {
+  if (_curAddress == _ownerAddress && _curAddress !== undefined && _ownerAddress!== undefined) {
+    var identity = document.getElementById("identity");
+    identity.innerHTML= "<br><div id = 'name'> " + _name + "</div> </br> ";
+    document.getElementById("adminPage").style.display = "block";
+    }
+  else {
+    var identity = document.getElementById("identity");
+    identity.innerHTML= "<br><div id = 'name'> " + _name + "</div> </br> ";
+  }
+};
+
+getLog();
+
+
+//update drop-down list
+//var select = document.getElementById("dest-select");
+
+const dropdownList = (_curAddress, _users) => {
+
+  var select = document.getElementById("dest-select");
+  for (var key in _users){
+	  if (_users.hasOwnProperty(key) && key !== "admin" && _users[key].address.toLowerCase() !== _curAddress.toLowerCase() && _users[key].address !== "0x0000000000000000000000000000000000000000") {
+      console.log(_users[key].address)
+      console.log(_users[key].name)
+      var opt = document.createElement('option');
+      opt.value = _users[key].address.toLowerCase();
+      opt.innerHTML = _users[key].name;
+      select.appendChild(opt);
+    }
+  }
+}
+
+const getUsers = async () =>{
+
+  let users = {};
+	let listAddress;
+	let name;
+	var i = 0;
+  
+	const getMembers = async () =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getMembers((err, members) => {
+				if (err) return reject(err);
+				resolve(members);
+	  	})
+	})}
+
+	const getName = async (address) =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getName(address, (err, name) => {
+				if (err) return reject(err);
+				resolve(name);
+		})
+  })}	
+  
+  const getTaille = async () =>{
+    return new Promise(function(resolve, reject){
+      Token.sizeListAccount((err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+    })
+  })}
+
+	listAddress = await getMembers();
+	console.log("get list of addresses")
+  console.log(listAddress);
+  let taille = await getTaille();
+	while (i < taille) {
+		var address = listAddress[i];
+		console.log(address)
+		name = await getName(address);
+		users[name]={}
+		users[name].address=address
+		users[name].name=name
+		i++
+		console.log(users[name].address)
+		console.log(users[name].name)
+  }
+  
+  //get current address before dropdownlist call, to remove own name from dropdown list
+  let curAddress;
+
+  const getCurAddress = async () =>{                         
+  return new Promise(function(resolve, reject){
+    web3.eth.getAccounts((err, accounts) => {
+      if (err) return reject(err);
+      resolve(accounts[0]);
+  })
+  })}
+
+
+  curAddress = await getCurAddress();
+  return dropdownList(curAddress, users);
+};
+
+getUsers();
