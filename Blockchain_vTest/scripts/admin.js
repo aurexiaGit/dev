072ed362ed6b7a464e015006af9c03725d83fe61
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-20 10:57:33 +0200

    test

--- Blockchain_vTest/scripts/admin.js
+++ Blockchain_vTest/scripts/admin.js
@@ -3,9 +3,6 @@ function initialDonations() {
 	// Called when the admin clicks on the button initial donations.
 	// The if is a security to prevent the admin to send donations by mistake
 	if (window.confirm("Do you really want to send initial donations to all Blockchain members?")) {
-		for (key in users) {
-			let amount = 50*Math.pow(10,18);
-			Token.transferAll(amount,(err,result) => {console.log("")}) // The initial donations were fixed at 50 AST
-		}
+		Token.transferAll(amount,(err,result) => {console.log("")}) // The initial donations were fixed at 50 AST
 	}
 }
