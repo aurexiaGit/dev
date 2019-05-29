e67930a60bc6c03702d17c7f4a2029f15e8eb329
spierreAurexia <sebastien.pierre@aurexia.com>
2019-05-29 18:11:29 +0200

    result array conole log

--- Blockchain_dev.1/scripts/history.js
+++ Blockchain_dev.1/scripts/history.js
@@ -54,7 +54,8 @@ const getHistory = async () =>{
 	//On ajoute et retire les parametres dans l'adresse afin d'avoir ce qu'on veut  "&ce_qu'on_veut=paramtre"
 	$.getJSON('https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=' + curAddress + '&contractaddress=0xD16A5ec89500Cf37EE825adBf3E4f0044466261a&startblock=0&endblock=999999999&sort=asc&apikey=NSAMUW521D6CQ63KHUPRQEERSW8FVRAF9B', function(data) {
 		var resultArray = data.result;
-
+		console.log("result Array");
+		console.log(resultArray);
 		// fill the history with data from json file. Required/relevant columns from json are:
 		//1) timeStamp (nb of seconds since 01/01/1970)
 		//2) from: originator of the transaction
