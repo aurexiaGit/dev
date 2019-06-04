3efe81c3c7084d1919b0e97e91e8f99eabb7b8b6
spierreAurexia <sebastien.pierre@aurexia.com>
2019-06-04 11:01:39 +0200

    ordre alpha et dropdown pour creation token

--- Blockchain_dev/scripts/adminToken.js
+++ Blockchain_dev/scripts/adminToken.js
@@ -76,3 +76,83 @@ const destroyTokens = async () => {
 
 	return true;
 }
+
+/***************************************************************** */
+/*                            Dropdown                             */
+/***************************************************************** */
+
+
+const dropdownListCreaRem = (_users, _keyName) => {  //on récupère l'addresse de l'utilisateur et l'objet javascript qui stockent les utilisateurs
+
+	var select = document.getElementById("from-select-crea");
+	for (let i=0; i<_keyName.length; i++){
+		key = _keyName[i];
+			if (_users.hasOwnProperty(key) && key !== "admin" && _users[key].address !== "0x0000000000000000000000000000000000000000") {
+			var opt = document.createElement('option');
+			opt.value = _users[key].address.toLowerCase();
+			opt.innerHTML = _users[key].name;
+			select.appendChild(opt);
+		}
+	}
+	var select = document.getElementById("from-select-rem");
+  	for (let i=0; i<_keyName.length; i++){
+		key = _keyName[i];
+			if (_users.hasOwnProperty(key) && key !== "admin" && _users[key].address !== "0x0000000000000000000000000000000000000000") {
+			var opt = document.createElement('option');
+			opt.value = _users[key].address.toLowerCase();
+			opt.innerHTML = _users[key].name;
+			select.appendChild(opt);
+		}
+	}
+}
+
+
+// Création d'un objet javascript qui stocke importe les utilisateurs du smartcontract
+const getUsersFrom = async () =>{
+  
+  let users = {}; //objet js
+	let listAddressAndName;
+	let name;
+	var i = 0;
+	
+	//fonction interagissant avec le SC recupérant une liste composé des noms et addresse des utilisateurs 
+	const getMembersAndName = async () =>{                        
+		return new Promise(function(resolve, reject){
+			Token.getMembersAndName((err, members) => {
+				if (err) return reject(err);
+				resolve(members);
+	  	})
+	})}
+	
+	//fonction interagissant avec le SC récupérant la taille de la liste 
+  const getTaille = async () =>{
+    return new Promise(function(resolve, reject){
+      Token.sizeListAccount((err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+    })
+  })}
+
+	//Création de l'objet JS users
+	listAddressAndName = await getMembersAndName();
+  let taille = await getTaille();
+	while (i < taille) {
+		var address = listAddressAndName[0][i];
+		name = web3.toAscii(listAddressAndName[1][i]);
+		users[name]={};
+		users[name].address=address;
+		users[name].name=name;
+		i++
+	}
+
+	let keyName = listAddressAndName[1];
+  for (let i=0; i<keyName.length; i++){
+    keyName[i]=web3.toAscii(keyName[i]);
+	}
+	keyName.sort();
+
+	//on retourne l'affichage du dropdown
+	return dropdownListCreaRem(users, keyName);
+};
+  
+getUsersFrom();
