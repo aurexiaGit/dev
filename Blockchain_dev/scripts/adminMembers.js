ddb740a00c229e1f39c8a65b9ce9e9ee7b76cf0d
spierreAurexia <sebastien.pierre@aurexia.com>
2019-06-18 15:34:13 +0200

    mass assignation in admin members

--- Blockchain_dev/scripts/adminMembers.js
+++ Blockchain_dev/scripts/adminMembers.js
@@ -41,24 +41,28 @@ function print (results) {
 	console.log(results.data)
 }
 
-$('#submit-file').on("click",function(e){
-	e.preventDefault();
-	$('#files').parse({
-		config: {
-			delimiter: "auto",
-			complete: print,
-		},
-		before: function(file, inputElem)
-		{
-			console.log("Parsing file...", file);
-		},
-		error: function(err, file)
-		{
-			console.log("ERROR:", err, file);
-		},
-		complete: function()
-		{
-			console.log("Done with all files");
-		}
+
+$(document).ready(function(){
+	$('#header').load('../header-ads.html');
+    $('#footer').load('../footer-ads.html');
+	$('#submit-file').on("click",function(e){
+		e.preventDefault();
+		$('#files').parse({
+			config: {
+				delimiter: "auto",
+				complete: print,
+			},
+			before: function(file, inputElem)
+			{
+				console.log("Parsing file...", file);
+			},
+			error: function(err, file)
+			{
+				console.log("ERROR:", err, file);
+			},
+			complete: function()
+			{
+				console.log("Done with all files");
+			}
+		});
 	});
-});
