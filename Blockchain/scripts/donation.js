e5247b05a13b3da55cc32b2a0cb0f5c30cb4a3c8
Soilhat MOHAMED <smohamed@aurexia.local>
2020-11-26 12:52:21 +0100

    test chart

--- Blockchain/scripts/donation.js
+++ Blockchain/scripts/donation.js
@@ -13,40 +13,37 @@ const dropdownListCharity = (_charity) => {
 	}
 }
 
+let charitiesNames= []
+let charitiesBalance=[]
 ////////////////////////////////////////////////////////
-//     Creation de la table listant les charity         //
+//     Creation de la table listant les charity         
 ////////////////////////////////////////////////////////
-
 
 //fonction permettant la création d'un tableau dynamique en html/CSS
 const getCharityTable = (_charity) => {
 	//ciblage de la balise html du tableau
 	var table = document.getElementById("content-donation")
 	var i = 1
-
-	console.log(_charity);
+	charitiesNames=[]
 	for (var key in _charity){
-		
 		//création de la nouvelle ligne
 		var row = document.createElement('tr');
 		row.class = "row" + i.toString() + " body";
 		table.appendChild(row);
-
 		//Remplissage des colonnes de la nouvelle ligne avec les valeurs
 		var column4 = document.createElement('td');
 		column4.contentEditable = "true";
 		column4.innerHTML = 0;
 		row.appendChild(column4)
-
 		var column2 = document.createElement('td');
 		column2.innerHTML = _charity[key].name;
+		charitiesNames.push(_charity[key].name);
 		row.appendChild(column2);
-
 		var column3 = document.createElement('td');
+		column3.style = "display:none;";
 		column3.innerHTML = _charity[key].address;
+		charitiesBalance.push(_charity[key].balance);
 		row.appendChild(column3);
-
-		console.log('is col editable : ' + column4.contentEditable)
 		i++
 	}
 }
@@ -69,10 +66,12 @@ const formatCharities = async() =>{
 	let taille = listCharity[0].length;
 	for (let i = 0; i < taille; i++) {
 		var address = listCharity[0][i];
-		let name = web3.toAscii(listCharity[1][i])
+		let name = web3.toAscii(listCharity[1][i]);
+		var balance = Math.round(listCharity[2][i] * Math.pow(10,-18));
 		charity[name]={}
 		charity[name].address=address.toLowerCase();
-		charity[name].name=name
+		charity[name].name=name;
+		charity[name].balance = balance;
 	}
 	return charity;
 }
@@ -105,9 +104,6 @@ getCharity();
 
 //fonction intéragissant avec le SC lorsqu'on appuie sur transfert. Elle active la fonction transferToAssociation du SC qui transfert tous les tokens de l'utilisateurs à l'association
 const transferCharity = async() => {
-	/*let amount = document.getElementById("amount").value
-	let address = document.getElementById("dest-select1").value
-	*/
 	
 	const transferEvent = async (_address, _amount) =>{
 		return new Promise(function(resolve, reject){
@@ -129,9 +125,40 @@ const transferCharity = async() => {
 			let address = cells[2].innerHTML
 			console.log("Send " + amount + " AST to " + name + " at address : " + address);
 			amount = amount*Math.pow(10,18);
+			await transferEvent(address, amount);
 		}
 	}
-
-	//let transferTransaction = await transferEvent(address, amount);
-	//return transferTransaction
 }
+
+let listCharity = await getCharities();
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+		labels: charitiesNames,
+		datasets: [{
+			label: 'Balance',
+			data: charitiesBalance,
+			backgroundColor: [
+			'rgba(255, 99, 132, 0.2)',
+			'rgba(54, 162, 235, 0.2)',
+			'rgba(255, 206, 86, 0.2)'
+			],
+			borderColor: [
+			'rgba(255,99,132,1)',
+			'rgba(54, 162, 235, 1)',
+			'rgba(255, 206, 86, 1)'
+			],
+			borderWidth: 1
+		}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
+});
